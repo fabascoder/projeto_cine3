2e50727236718400802a869055f90cce1b628e52
fabascorder <fabricialvez@gmail.com>
2024-10-22 16:18:52 -0300

    mudando a conexao

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; 
-$db = "projetocine31";
-$user = "root";
-$senha = "";
+$host = "sql205.infinityfree.com"; 
+$db = "if0_37567896_banco";
+$user = "if0_37567896";
+$senha = "fabricio124 ";
  
 try {
     $conexao = new PDO("mysql:host=$host;dbname=$db", $user, $senha);
