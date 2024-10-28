ae78eb7da516f9b4e07f25e6472a1e8eb0121b1a
arturqueiiroz <arturos.queiroz@gmail.com>
2024-10-28 16:57:33 -0300

    alteracoes

--- local.php
+++ local.php
@@ -45,9 +45,6 @@
             </form>
             <?php
             $cep = $_POST['cep']??'';
-            $url = "https://viacep.com.br/ws/$cep/json/";
-            $json = file_get_contents($url);
-            $dados = json_decode($json, true);
             
             ?>
             <div id="resposta"></div>
