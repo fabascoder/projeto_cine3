38934d8aebd65077c75256232f2958aa5fd8db25
fabascoder <fabricialvez@gmail.com>
2024-10-29 17:03:42 -0300

    colocando php nas paginas de pagamento

--- pagamento_produtos.php
+++ pagamento_produtos.php
@@ -7,50 +7,9 @@
     <link rel="stylesheet" href="css/pagamento_produtos.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">
-                        <img src="imagens/Logo_Cine3-removebg-preview.png" alt="Cine3 Logo" class="logo">
-                    </a>
-                </li>   
-                
-                <li>     
-                    <a href="">Olá, [Nome]</a>
-                </li>
-            </ul>
-            <a href="index.php"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
-        </nav>
-        <div id="caixa"> 
-            <div class="etapas-compra">
-                <img src="imagens/ingressos.png" alt="">
-                
-                <p>Ingresso</p>
-            </div>
-            
-            <div class="etapas-compra">
-                <img src="imagens/secao.png" alt="">
-                
-                <p>Seção</p>
-            </div>
-            <div class="etapas-compra">
-                <img src="imagens/pipoca.png" alt="">
-                
-                <p>Produtos</p>
-            </div>
-            <div class="etapas-compra">
-                <img src="imagens/cartao.png" alt="">
-                
-                <p>Pagamento</p>
-            </div>
-            <div class="etapas-compra">
-                <img src="imagens/confirmacao.png" alt="">
-                
-                <p>Confirmação</p>
-            </div>
-        </div>
-    </header>
+<?php 
+    include_once "header_pagto.php";
+    ?>
     <main>
             <div class="caixa-principal">
                 <h1>PRODUTOS E COMBOS</h1>
