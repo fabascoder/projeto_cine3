3cc18bcb7a5779e3652ab73e05a56fa1bfbbcc3a
arturqueiiroz <arturos.queiroz@gmail.com>
2024-11-05 14:24:48 -0300

    Adicionado js em pagamento_ingresso

--- javascript/pagamento_ingressos.js
+++ javascript/pagamento_ingressos.js
@@ -9,3 +9,20 @@ function decrementa(id) {
         input.value = parseInt(input.value) - 1;
     }
 }
+
+function calcularTotal() {
+    const precoInteira = 38.00;
+    const precoMeia = 19.00;
+
+    const quantidadeInteira = document.getElementById('quantidadeInteira').value;
+    const quantidadeMeia = document.getElementById('quantidadeMeia').value;
+
+    const totalInteira = quantidadeInteira * precoInteira;
+    const totalMeia = quantidadeMeia * precoMeia;
+    const totalPagar = totalInteira + totalMeia;
+
+    document.getElementById('totalInteira').textContent = `R$ ${totalInteira.toFixed(2)}`;
+    document.getElementById('totalMeia').textContent = `R$ ${totalMeia.toFixed(2)}`;
+    document.getElementById('total_pagar_display').textContent = `TOTAL À PAGAR R$ ${totalPagar.toFixed(2)}`;
+    document.getElementById('total_pagar').value = totalPagar.toFixed(2);
+}
