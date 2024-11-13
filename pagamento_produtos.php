b501e98d97e2c1626a81387a8cd43273af24fe55
Pedro Stuart <pedrohsmoura897@gmail.com>
2024-11-13 14:17:13 -0300

    .

--- pagamento_produtos.php
+++ pagamento_produtos.php
@@ -8,8 +8,52 @@
     <link rel="stylesheet" href="css/footer.css">
 </head>
 <body>
+    <header>
+    <nav>
+            <ul>
+                <li>
+                    <a href="index.php">
+                        <img src="imagens/Logo_Cine3-removebg-preview.png" alt="Cine3 Logo" class="logo">
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">Olá, [Nome]</a>
+                </li>
+            </ul>
+            <a href="index.php"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+        </nav>
+        <div id="caixa">
+            <div class="etapas-compra">
+                <img src="imagens/ingressos.png" alt="">
+
+                <p>Ingresso</p>
+            </div>
+
+            <div class="etapas-compra">
+                <img src="imagens/secao.png" alt="">
+
+                <p>Seção</p>
+            </div>
+            <div class="etapas-compra3">
+                <img src="imagens/pipoca.png" alt="">
+
+                <p>Produtos</p>
+            </div>
+            <div class="etapas-compra">
+                <img src="imagens/cartao.png" alt="">
+
+                <p>Pagamento</p>
+            </div>
+            <div class="etapas-compra">
+                <img src="imagens/confirmacao.png" alt="">
+
+                <p>Confirmação</p>
+            </div>
+        </div>
+    </header>
 <?php 
-    include_once "header_pagto.php";
+    
     session_start();
     $_SESSION['local'] = $_POST['local_sessao'];
     $_SESSION['assentos'] = [];
--- pagamento_sessao.php
+++ pagamento_sessao.php
@@ -8,7 +8,6 @@
 </head>
 <body>
 <?php 
-    include_once "header_pagto.php";
     $_SESSION['total_pagar'] = $_POST['total_pagar']??"";
     $_SESSION['total'] = $_POST['total']??"";
     $_SESSION['qtd_inteira'] = $POST['qtd_inteira']??"";
@@ -18,7 +17,48 @@
     $_SESSION['tamanho-camiseta'] = $_POST['tamanho-camiseta']??"";
     ?>
     <header>
-        <!-- Código do header omitido para simplificação -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">
+                        <img src="imagens/Logo_Cine3-removebg-preview.png" alt="Cine3 Logo" class="logo">
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">Olá, [Nome]</a>
+                </li>
+            </ul>
+            <a href="index.php"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+        </nav>
+        <div id="caixa">
+            <div class="etapas-compra">
+                <img src="imagens/ingressos.png" alt="">
+
+                <p>Ingresso</p>
+            </div>
+
+            <div class="etapas-compra2">
+                <img src="imagens/secao.png" alt="">
+
+                <p>Seção</p>
+            </div>
+            <div class="etapas-compra">
+                <img src="imagens/pipoca.png" alt="">
+
+                <p>Produtos</p>
+            </div>
+            <div class="etapas-compra">
+                <img src="imagens/cartao.png" alt="">
+
+                <p>Pagamento</p>
+            </div>
+            <div class="etapas-compra">
+                <img src="imagens/confirmacao.png" alt="">
+
+                <p>Confirmação</p>
+            </div>
+        </div>
     </header>
 
     <main>
